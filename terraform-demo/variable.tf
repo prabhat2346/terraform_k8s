feeab1c27ab5167c44424f0a184a65726b185a45
prabhat2346 <44159355+prabhat2346@users.noreply.github.com>
2021-06-03 02:50:05 +0530

    terraform_count.index

---
+++ terraform-demo/variable.tf
@@ -0,0 +1,38 @@
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+# variable used for ELB(target)
+variable "target_group" {}
+variable "LB_Evelyn2_Dev" {}
+variable "LB_Evelyn2_Dev_Internal" {}
+variable "Evelyn2_Dev_ELB_Alarm_Name" {}
+variable "Evelyn2_Dev_Internal_ELB_Alarm_Name" {}
+variable "Evelyn2_Dev_Availability_zone" {}
+variable "LB_Evelyn2_Dev_4xx" {}
+variable "LB_Evelyn2_Dev_Internal_4xx" {}
+variable "Evelyn2_Dev_ELB_Alarm_Name_4xx" {}
+variable "Evelyn2_Dev_Internal_ELB_Alarm_Name_4xx" {}
+variable "Evelyn2_Dev_Availability_zone_4xx" {}
+# for ELB 5xx
+variable "LB_Evelyn2_Dev_elb" {}
+variable "LB_Evelyn2_Dev_Internal_elb" {}
+variable "Evelyn2_Dev_ELB_Alarm_Name_elb" {}
+variable "Evelyn2_Dev_Internal_ELB_Alarm_Name_elb" {}
+variable "Evelyn2_Dev_Availability_zone_elb" {}
+# for ELB 4xx
+variable "LB_Evelyn2_Dev_4xx_elb" {}
+variable "LB_Evelyn2_Dev_Internal_4xx_elb" {}
+variable "Evelyn2_Dev_ELB_Alarm_Name_4xx_elb" {}
+variable "Evelyn2_Dev_Internal_ELB_Alarm_Name_4xx_elb" {}
+variable "Evelyn2_Dev_Availability_zone_4xx_elb" {}
+
+#  # ELB target Resoponse time.
+
+
+variable "LB_Evelyn2_Dev_trt" {}
+ variable "LB_Evelyn2_Dev_Internal_trt" {}
+variable "Evelyn2_Dev_ELB_Alarm_Name_trt" {}
+ variable "Evelyn2_Dev_Internal_ELB_Alarm_Name_trt" {}
+variable "Evelyn2_Dev_Availability_zone_trt" {}
+
+
